c9970838f4d0417c69a176d34ec3fc4d38248b1b
calvinchung123 <C15529113@mytudublin.ie>
2020-04-21 16:57:05 +0100

    Initial commit

---
+++ Assets/TrafficLights.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLights : MonoBehaviour
+{
+    // Instantiates prefabs in a circle formation
+    public GameObject prefab;
+    Material m_Material;
+    public int numberOfObjects = 10;
+    public float radius = 5f;
+    private MeshRenderer meshRenderer;
+    float duration = 1.0f;
+    Color color0 = Color.red;
+    Color color1 = Color.green;
+    void Start()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            float x = Mathf.Cos(angle) * radius;
+            float z = Mathf.Sin(angle) * radius;
+            Vector3 pos = transform.position + new Vector3(x, 0, z);
+            float angleDegrees = -angle * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+            Instantiate(prefab, pos, rot);
+            /*prefab.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color", Color.green);
+            meshRenderer = GetComponent<MeshRenderer>();*/
+            /* prefab.material.color = Color.red;*/
+        }
+        prefab = GetComponent<GameObject>();
+        m_Material = GetComponent<Renderer>().material;
+    }
+
+    void update()
+    {
+        
+    }
+}
---
+++ Assets/Trafficlight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trafficlight : MonoBehaviour
+{
+    // Instantiates prefabs in a circle formation
+    public GameObject prefab;
+    public int numberOfObjects = 10;
+    public float radius = 5f;
+    private MeshRenderer meshRenderer;
+    private float timeToChange = 0.1f;
+    private float timeSinceChange = 0f;
+    void Start()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            float x = Mathf.Cos(angle) * radius;
+            float z = Mathf.Sin(angle) * radius;
+            Vector3 pos = transform.position + new Vector3(x, 0, z);
+            float angleDegrees = -angle * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+            Instantiate(prefab, pos, rot);
+            prefab.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_Color", Color.green);
+            meshRenderer = GetComponent<MeshRenderer>();
+            /* prefab.material.color = Color.red;*/
+        }
+    }
+
+    void update()
+    {
+
+            /*if(meshRenderer !=)*/
+    }
+}
